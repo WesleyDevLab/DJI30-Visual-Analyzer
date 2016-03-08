af1d52bf9cc6e8d2cd5b9c30daf20be066bd6aa5
Zhendong Cao <ryecao@gmail.com>
2016-03-08 23:49:00 +0800

    move from browserify to webpack

---
+++ app/views/components/parallel-coordinates.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var d3 = require('d3');
+
+var ParallelCoordinates = React.createClass({
+        render: function() {
+            return null;
+        }
+});
+
+module.exports = ParallelCoordinates;
